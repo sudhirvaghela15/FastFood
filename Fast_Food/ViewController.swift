49ae10c3d8070ef4b1aa63baad9cabfdecd8a9dd
sudhir <sudhirvaghela15@gmail.com>
2021-05-11 11:57:08 +0530

    Initial Commit

---
+++ Fast_Food/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Fast_Food
+//
+//  Created by sudhir on 11/05/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
